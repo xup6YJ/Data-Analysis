b139331160726fe0feb4be3799189d5636e4e9cf
Lin Y J <74194952+xup6YJ@users.noreply.github.com>
2022-04-24 00:04:38 +0800

    Update PSM.R

--- Code/PSM.R
+++ Code/PSM.R
@@ -3,23 +3,6 @@
 mydata <- read.csv('c_1009.csv')
 
 library(MatchIt)
-
-#######Siegle
-colnames(mydata)
-
-m.out = matchit (中醫 ~ 性別 + Age + 糖尿病 + 高血壓 + siegle0 + 耳鳴 + 暈眩 + time_onset, 
-                 data = mydata, method = "nearest", ratio = 1, caliper = 0.2)
-
-# matchit
-result = summary (m.out)
-# plot (m.out, type = "jitter")
-plot (m.out, type = "hist")
-
-m.data1 <- match.data (m.out)
-write.csv (m.data1, "match.csv")
-
-
-## tableone package itself
 library(tableone)
 ## PS matching
 library(Matching)
@@ -30,15 +13,30 @@ library(reshape2)
 ## plotting
 library(ggplot2)
 
-data1 = read.csv("M_match.csv")
+colnames(mydata)
+
+m.out = matchit (chinese_med ~ sex + age + diabetes + hypertension + time_onset, 
+                 data = mydata, method = "nearest", ratio = 1, caliper = 0.2) #caliper = tolerance
+
+# matchit
+result = summary (m.out)
+# plot (m.out, type = "jitter")
+plot (m.out, type = "hist")
+
+m.data1 <- match.data (m.out)
+# write.csv (m.data1, "match.csv")
+
+# m.data1 = read.csv("match.csv")
 colnames(data1)
 
 ## Covariates
+#continuous var
 vars <- c('time_onset' ,'siegle', 'SDRG', )
+#factorial var
 fvars = c('sex' ,'hypertension')
 
 ## Construct a table
-tabUnmatched <- CreateTableOne(vars = vars, strata = "Chinese_med", data = data1, test = TRUE, factorVars = fvars)
+tabUnmatched <- CreateTableOne(vars = vars, strata = "Chinese_med", data = m.data1, test = TRUE, factorVars = fvars)
 ## Show table with SMD
 x1 = print(tabUnmatched)
 x = print(tabUnmatched, smd = TRUE)
