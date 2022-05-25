8d1929923fe6cf55ef2927395f3378ffe93a073e
Lin Y J <74194952+xup6YJ@users.noreply.github.com>
2022-05-25 21:01:23 +0800

    Update Survival.R

--- Code/Survival.R
+++ Code/Survival.R
@@ -4,6 +4,9 @@ library("survival")
 
 data("ovarian")
 
+#calculate censoring data(days)
+mydata[i, 61] = difftime(as.Date(mydata[i, 50]), as.Date(mydata[i, 41]), units = "days")
+
 exp(-predict(fit,  type="expected"))
 exp(-predict(fit, data = ovarian2, type="expected"))
 
@@ -15,6 +18,7 @@ test = read.csv('test.csv')
 
 fit <- coxph(Surv(Acute_LOS, X30_RA) ~ ., data = train)
 
+#probability of model prediction
 prob = exp(-predict(fit, data = test, type="expected"))
 prob <- exp(-predict(fit, newdata = test,type = "expected"))
 
