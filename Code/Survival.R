fb237b86238215439331dc54f77adb2d7b6a9e1a
Lin Y J <74194952+xup6YJ@users.noreply.github.com>
2022-05-25 15:05:45 +0800

    Update Survival.R

--- Code/Survival.R
+++ Code/Survival.R
@@ -7,8 +7,8 @@ data("ovarian")
 exp(-predict(fit,  type="expected"))
 exp(-predict(fit, data = ovarian2, type="expected"))
 
-train = read.csv('D:/File_X/Help/hsu/train.csv')
-test = read.csv('D:/File_X/Help/hsu/test.csv')
+train = read.csv('train.csv')
+test = read.csv('test.csv')
 
 # event : 30_RA
 # time : Acute_LOS
@@ -18,7 +18,7 @@ fit <- coxph(Surv(Acute_LOS, X30_RA) ~ ., data = train)
 prob = exp(-predict(fit, data = test, type="expected"))
 prob <- exp(-predict(fit, newdata = test,type = "expected"))
 
-write.csv(prob, 'D:/File_X/Help/hsu/test_prob.csv')
+write.csv(prob, 'test_prob.csv')
 
 
 
